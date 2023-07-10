6008dadf9472cb5ea18220eb91e6b2ef30cb9289
Josh Sawyer <thejoshsawyer@gmail.com>
2023-07-09 23:18:57 -0700

    Add Board class

---
+++ lib/knights-travails/board.rb
@@ -0,0 +1,12 @@
+class Board
+  attr_accessor :to_visit, :visited
+
+  def initialize
+    @to_visit = []
+    @visited = []
+  end
+
+  def add_square(square)
+    to_visit.append(square)
+  end
+end
