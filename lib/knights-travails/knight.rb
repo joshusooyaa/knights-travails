c9446f5025a4e83e4cf59c6c8546da7281af5f1e
Josh Sawyer <thejoshsawyer@gmail.com>
2023-07-10 11:39:28 -0700

    Update with small refactor

--- lib/knights-travails/knight.rb
+++ lib/knights-travails/knight.rb
@@ -1,8 +1,6 @@
 require_relative './node'
 
 class Knight
-  attr_accessor :to_visit, :visited
-
   def initialize(board)
     @board = board
     @to_visit = []
@@ -12,16 +10,13 @@ class Knight
 
   def shortest_path(start, destination)
     current_square = Node.new(start)
-    destination_square = Node.new(destination)
-    @visited.append(current_square)
 
-    loop do
-      return trace_path(current_square) if current_square.square == destination_square.square
-
-      append_reachable_squares(current_square)
+    until current_square.square == destination
+      add_reachable_squares(current_square)
       @visited.append(current_square)
       current_square = @to_visit.shift
     end
+    trace_path(current_square)
   end
 
   private
@@ -32,24 +27,34 @@ class Knight
 
   def trace_path(square)
     path = ''
+    path_size = -1
     until square.nil?
       path.concat("#{square.square}\n")
       square = square.parent
+      path_size += 1
     end
+    puts "Shortest path is #{path_size} moves!"
     path
   end
 
-  def append_reachable_squares(square)
+  def add_reachable_squares(square)
     row = square.square.ord - 65
     column = square.square[1].to_i - 1
 
     @moves.each do |move|
-      row_direction = row + move[0]
-      column_direction = column + move[1]
-      unless row_direction.negative? || column_direction.negative?
-        new_square = @board.board[row_direction][column_direction]
-        @to_visit.append(Node.new(new_square, square)) unless new_square.nil? || @visited.include?(new_square)
-      end
+      new_row = row + move[0]
+      new_column = column + move[1]
+
+      add_to_visit(square, new_row, new_column) if within_bounds?(new_row, new_column)
     end
+  end
+
+  def within_bounds?(row, column)
+    row.between?(0, 7) && column.between?(0, 7)
+  end
+
+  def add_to_visit(square, row, column)
+    new_square = @board.board[row][column]
+    @to_visit.append(Node.new(new_square, square)) unless @visited.include?(new_square)
   end
 end
--- lib/knights_travails.rb
+++ lib/knights_travails.rb
@@ -8,4 +8,17 @@ def knight_moves(start, destination)
   puts knight.shortest_path(start, destination)
 end
 
-knight_moves('A1', 'B1')
+def user_choice
+  response = gets.chomp.upcase
+  until response[0].between?('A', 'H') && response[1]&.between?('1', '8') && response.length == 2
+    response = gets.chomp.upcase
+  end
+  response
+end
+
+puts "Choose the knight's starting square."
+start = user_choice
+puts "Choose the knight's destination square."
+destination = user_choice
+
+knight_moves(start, destination)
