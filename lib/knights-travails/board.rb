f015eaabe10046f4571d7c1526b0bde503aeca49
Josh Sawyer <thejoshsawyer@gmail.com>
2023-07-09 23:27:40 -0700

    Add Board class

--- lib/knights-travails/board.rb
+++ lib/knights-travails/board.rb
@@ -1,3 +1,19 @@
 class Board
-  
+  attr_accessor :board
+
+  def initialize
+    @board = create_board
+  end
+
+  private
+
+  def create_board
+    board = Array.new(8) { Array.new(8) }
+    ('A'..'H').each_with_index do |letter, i|
+      ('1'..'8').each_with_index do |number, j|
+        board[i][j] = "#{letter}#{number}"
+      end
+    end
+    board
+  end
 end
